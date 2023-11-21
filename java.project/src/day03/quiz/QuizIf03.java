13084accd768119aa23ddc0348356fdde3e7dadd
dhkdtld37 <hyeonls96@naver.com>
2023-11-21 17:49:07 +0900

    2023. 11. 21. pm 5.47

--- java.project/src/day03/quiz/QuizIf03.java
+++ java.project/src/day03/quiz/QuizIf03.java
@@ -2,11 +2,26 @@
   문제 3 ] 전산, 영어, 국어 점수 입력 받아서 평균 60점 이상이고,
 			각 과목 과락 40점 이하이면 과목 불합격 처리하는 프로그램 작성
 */
-package day03.quiz;
+
+ package day03.quiz;
+ 
+
+import java.util.Scanner;
 
 public class QuizIf03 {
 	public static void main(String[] args) {
+		System.out.println(100);
+	}
+}
+		
+		
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		System.out.println("전산 점수를 입력하시오");
+		int a = sc.nextInt();
 		
 	}
 
 }
+*/
