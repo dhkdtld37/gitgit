1fa51ecbdba48b34bf0de4e4c0e82bda3f114f40
dhkdtld37 <hyeonls96@naver.com>
2023-12-05 10:43:16 +0900

    2023. 12. 05. am. 10. 43.

---
+++ java.project/src/day11/quiz/collection/list02/Customer.java
@@ -0,0 +1,24 @@
+package day11.quiz.collection.list02;
+
+public class Customer { // model
+	protected String name;
+	protected String add;
+	protected String tel;
+
+	Customer() {
+	}
+
+	Customer(String name, String add, String tel) {
+		this.name = name;
+		this.add = add;
+		this.tel = tel;
+	}
+
+	public void display() {
+		System.out.println("이름    : " + name);
+		System.out.println("주소    : " + add);
+		System.out.println("연락처 : " + tel);
+	}	// display()
+
+}	// Customer
+
